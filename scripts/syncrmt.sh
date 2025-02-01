ba0bbdd0d3a7f43fc010a278f217337eb3df9430
Jiamu Sun <barroit@linux.com>
2025-02-01 10:43:08 +0900

    scripts: syncrmt: add script to sync remote sqlite
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/syncrmt.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+if git remote | grep -q sqlite; then
+	git switch sqlite
+	git pull sqlite master
+else
+	url=$(cat .upstream)
+
+	git remote add sqlite $url
+	git fetch sqlite master
+	git checkout -b sqlite sqlite/master
+fi
+
+git switch master
+git rebase --onto sqlite $(git merge-base master sqlite) master
